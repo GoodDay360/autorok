01d85dca8acaeb009fe76a9426f52894a18da9e4
unknown <istartgame31@gmail.com>
2025-08-01 12:28:01 +0700

    fix dependencies+bug

--- build.rs
+++ build.rs
@@ -1,17 +1,23 @@
 use {
-    std::{
-        env,
-        io,
-    },
+    std::{env, io},
     winresource::WindowsResource,
 };
 
 fn main() -> io::Result<()> {
-    if env::var_os("CARGO_CFG_WINDOWS").is_some() {
-        WindowsResource::new()
-            // This path can be absolute, or relative to your crate root.
-            .set_icon("resource/icon.ico")
-            .compile()?;
+    let target = env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
+
+    if cfg!(debug_assertions) == false { 
+        if target == "windows"{
+            let mut res = WindowsResource::new();
+            res.set_manifest_file("app.window.manifest");
+            res.set_icon("resource/icon.ico");
+            res.compile()?;
+            println!("Embedding Windows manifest + icon...");
+        } else {
+            println!("Skipping Windows-specific resource embedding for OS: {target}");
+        }
     }
+    
+
     Ok(())
 }
